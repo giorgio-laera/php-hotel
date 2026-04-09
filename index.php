fec6a0607546b76ceb0c43584482f3acdb6c070a
giorgio laera <laeragiorg@gmail.com>
2026-04-09 19:09:39 +0200

    aggiuto boostrap con relativo style

--- index.php
+++ index.php
@@ -1,64 +1,88 @@
+
+<?php
+    $hotels = [
+
+    [
+        'name'               => 'Hotel Belvedere',
+        'description'        => 'Hotel Belvedere Descrizione',
+        'parking'            => true,
+        'vote'               => 4,
+        'distance_to_center' => 10.4,
+    ],
+    [
+        'name'               => 'Hotel Futuro',
+        'description'        => 'Hotel Futuro Descrizione',
+        'parking'            => true,
+        'vote'               => 2,
+        'distance_to_center' => 2,
+    ],
+    [
+        'name'               => 'Hotel Rivamare',
+        'description'        => 'Hotel Rivamare Descrizione',
+        'parking'            => false,
+        'vote'               => 1,
+        'distance_to_center' => 1,
+    ],
+    [
+        'name'               => 'Hotel Bellavista',
+        'description'        => 'Hotel Bellavista Descrizione',
+        'parking'            => false,
+        'vote'               => 5,
+        'distance_to_center' => 5.5,
+    ],
+    [
+        'name'               => 'Hotel Milano',
+        'description'        => 'Hotel Milano Descrizione',
+        'parking'            => true,
+        'vote'               => 2,
+        'distance_to_center' => 50,
+    ],
+
+    ];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <h1>I nostri Hotel</h1>
+<table class="table">
    <?php
 
-    $hotels = [
+       foreach ($hotels as $hotel) {
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+           echo "<div class='d-flex'>";
 
-    ];
+           foreach ($hotel as $key => $value) {
 
- foreach ($hotels as $hotel){
-    foreach ($hotel as $value){
+               echo "$key";
 
-        echo "$value ";
+           }
 
-    };
+           echo "</div>";
 
-    echo "<br>";
-    
- };
+       }
 
-?>
+       foreach ($hotels as $hotel) {
+
+           echo "<div class='d-flex'>";
+
+           foreach ($hotel as $key => $value) {
+
+               echo "$key  $value ";
+
+           }
+
+           echo "</div>";
+
+       }
+
+   ?>
+</table>
     </body>
 </html>
