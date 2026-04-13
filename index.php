6dadf808877c08235d29b1f4befdba69e8ecd75e
giorgio laera <laeragiorg@gmail.com>
2026-04-13 18:31:16 +0200

    aggiunto filtro per voto

--- index.php
+++ index.php
@@ -40,6 +40,23 @@
 
     ];
 ?>
+<?php
+    $parkingIs = false;
+    if (isset($_GET['parking']) && $_GET['parking'] == 'on') {
+    $parkingIs = true;
+    }
+    var_dump($parkingIs);
+    ?>
+    
+    <?php
+// controllo ricerca per stelle
+$min_star= 0;
+      if (isset($_GET['star'])  && $_GET['star'] < 5) {
+    $min_star = $_GET['star'];
+    };
+    var_dump($min_star);
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -51,27 +68,49 @@
 </head>
 <body>
     <h1>I nostri Hotel</h1>
+    <form action="">
+        <div class='d-flex'>
+            <div class="form-control">
+                <input class="form-check-input" id ="parking" name="parking" type="checkbox">
+                <label class="form-check-label" for="parking">Presenza parcheggio</label>
+            </div>
+
+            <div class=" ms-3 form-control">
+                <input type="number" min= "0" max= "5" id="star" name="star">
+                <label for="star">Stelle</label>
+            </div>
+        </div>
+        <button class="btn btn-dark">invia</button>
+    </form>
+
 <table class="table">
     <thead>
-    <tr>
-      <th scope="col">name</th>
-      <th scope="col">description</th>
-      <th scope="col">parking</th>
-      <th scope="col">vote</th>
-      <th scope="col">distance_to_center</th>
-    </tr>
-  </thead>
+        <tr>
+            <th scope="col">name</th>
+            <th scope="col">description</th>
+            <th scope="col">parking</th>
+            <th scope="col">vote</th>
+            <th scope="col">distance_to_center</th>
+        </tr>
+    </thead>
        <tBody>
     <?php
-       foreach ($hotels as $hotel) {
+
+        foreach ($hotels as $hotel) {
+            if ($parkingIs === true && $hotel["parking"] === false) {
+                continue;
+            }
+            if ($hotel['vote'] < $min_star){
+                continue;
+            };
             echo "<tr>";
             foreach ($hotel as $key => $value) {
 
-                if ($key== 'parking' && $value == true ){
+                if ($key == 'parking' && $value == true) {
                     echo '<td>yes</td>';
-                } else if ($key== 'parking' && $value == false ){
+                } else if ($key == 'parking' && $value == false) {
                     echo '<td>no</td>';
-                }else{
+                } else {
 
                     echo "<td>$value </td>";
                 }
@@ -80,7 +119,7 @@
 
             echo "</tr>";
 
-        };
+        }
 
     ?>
    </tBody>
